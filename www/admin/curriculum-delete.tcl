dc2bb1cbb09d9c4003a8f96e4cd0e7c25086a3cd
Ola Hansson <ola@polyxena.net>
2003-09-22 20:51:08 +0000

    Flush the element cache when a curriculum is deleted from the admin UI.

--- www/admin/curriculum-delete.tcl
+++ www/admin/curriculum-delete.tcl
@@ -13,8 +13,6 @@ ad_page_contract {
 curriculum::delete -curriculum_id $curriculum_id
 
 # Force the curriculum bar to reflect the reality.
-# Not needed since we do this when we disable a curriculum,
-# which we always do before we delete it.
-#curriculum::elements_flush
+curriculum::elements_flush
 
 ad_returnredirect "."
